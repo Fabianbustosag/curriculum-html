c2928e6894b4f70fc5ded6d43b9f0bb1c4370cd8
Fabian Bustos <fabianbustosag@gmail.com>
2026-01-24 11:50:59 -0300

    anadir formato 2

---
+++ F2/data.js
@@ -0,0 +1,53 @@
+const data = {
+  name: "Fabian Bustos",
+  title: "Computer Engineer",
+  contact: {
+    location: "Concepción, Biobío, Chile",
+    phone: "926222479",
+    email: "fabianbustosag@gmail.com",
+  },
+  professional_profile:
+    "Profesional con más de un año de experiencia en desarrollo de software, acumulando un total de 19 meses de trayectoria laboral en entornos empresariales. He participado en proyectos de gestión de flotas con soluciones GPS y ERP (facturación, RRHH y contabilidad). Colaboré en iniciativas urbanas del Gobierno Regional del Biobío (Chile) y del Ministerio, enfocadas en estudios de urbanismo y planificación territorial. Además, cuento con experiencia administrativa en la gestión operativa de flotas en una empresa de telecomunicaciones (CAM).",
+  work_experience: [
+    {
+      position: "Software Developer Intern",
+      company: "CityLab Biobío",
+      period: "Sep 2022 — Feb 2023",
+      location: "Concepción",
+      responsabilities: [
+        "Desarrollo y mantenimiento de módulos ERP en áreas como Recursos Humanos, Facturación, Logística y Seguimiento Satelital, utilizando Laravel y CodeIgniter con PHP bajo arquitectura MVC.",
+        "Creación de funciones en PL/pgSQL, implementación de triggers y optimización de consultas para mejorar el rendimiento y generación de reportes en PostgreSQL.",
+        "Diseño y mejora de interfaces con JavaScript y Bootstrap, aplicando buenas prácticas para asegurar una experiencia accesible y fácil de usar.",
+        "Lideré y participé en un proyecto de gestión logística para un cliente, ayudando a definir la estructura del sistema, los flujos de trabajo y documentando de forma clara, además de realizar pruebas para asegurar calidad.",
+      ],
+    },
+    {
+      position: "Software Developer Intern",
+      company: "CityLab Biobío",
+      period: "Sep 2022 — Feb 2023",
+      location: "Concepción",
+      responsabilities: [
+        "Desarrollo y mantenimiento de módulos ERP en áreas como Recursos Humanos, Facturación y Contabilidad.",
+        "Diseño y optimización de interfaces de usuario con JavaScript y Bootstrap, priorizando la usabilidad y experiencia del usuario.",
+        "Implementación de lógica de negocio backend con PHP y PostgreSQL, integrando y mejorando procesos del sistema.",
+        "Corrección de errores, análisis de incidentes y optimización del rendimiento en distintas capas del software.",
+        "Participación en el diseño funcional y estructural del sistema, contribuyendo a la definición de flujos y arquitectura.",
+        "Soporte técnico en terreno para la instalación y mantenimiento de dispositivos GPS, coordinando con clientes y personal técnico.",
+      ],
+    },
+  ],
+  hard_skills: ["PHP", "JAVA", "PYTHON"],
+  education: [
+    {
+      period: "Mar 2021 — Dec 2025",
+      degree: "Bachiller en Ciencias de la Ingeniería en Computación",
+      institution: "Universidad Técnica Federico Santa María",
+    },
+    {
+      period: "Jan 2016 — Dec 2019",
+      degree:
+        "Licencia de Enseñanza Media / Técnico en Administración de Empresas",
+      institution: "Centro Educacional Evangélico Hualpén",
+    },
+  ],
+};
