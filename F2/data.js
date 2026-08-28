628cb0bd668dece5e2e0f0cd1d30ba214688f994
Fabian Bustos <fabianbustosag@gmail.com>
2026-08-27 20:46:20 -0400

    data para php

--- F2/data.js
+++ F2/data.js
@@ -1,13 +1,13 @@
 const data = {
   name: "Fabian Bustos",
-  title: "Ingeniero Informático",
+  title: "Desarrollador de Software",
   contact: {
     location: "Chile",
     phone: "926222479",
     email: "fabianbustosag@gmail.com",
   },
   professional_profile:
-       "Desarrollador de software con más de un año de experiencia en sistemas ERP, especializado en el desarrollo y mantenimiento de aplicaciones en PHP (Laravel, CodeIgniter). Experiencia desarrollando soluciones orientadas a procesos empresariales. Manejo de bases de datos PostgreSQL, optimización de consultas y procesamiento de información. Con conocimientos en JavaScript y Python como apoyo en el desarrollo, además de conocimientos básicos en testing de software.",
+       "Desarrollador de software con más de un año de experiencia en sistemas ERP, especializado en el desarrollo y mantenimiento de aplicaciones en PHP (Laravel, CodeIgniter). Experiencia en lógica de negocio en el área de RRHH e integraciones con el SII. Manejo de bases de datos PostgreSQL, optimización de consultas y procesamiento de información. API Rest Node, además de conocimientos básicos en testing de software.",
   work_experience: [
     {
       position: "Desarrollador de Software",
@@ -15,12 +15,13 @@ const data = {
       period: "Ene 2025 — Presente",
       location: "Concepción",
       responsabilities: [
-        "Desarrollo y mantenimiento de módulos ERP utilizando Laravel y CodeIgniter.",
-        "Creación de funciones en PL/pgSQL, implementación de triggers, vistas y optimización de consultas para mejorar el rendimiento y generación de reportes en PostgreSQL.",
-        "Diseño y mejora de interfaces con JavaScript y Bootstrap, aplicando buenas prácticas para asegurar una experiencia accesible y fácil de usar.",
-        "Lideré y participé en un proyecto de gestión logística para un cliente, ayudando a definir la estructura del sistema, los flujos de trabajo y documentando de forma clara, además de realizar pruebas para asegurar calidad.",
+        "Desarrollo y mantenimiento de módulos ERP en áreas como Recursos Humanos, Facturación, Logística y Seguimiento Satelital, utilizando Laravel y CodeIgniter.",
+        "Creación de funciones en PL/pgSQL, implementación de vistas y optimización de consultas para mejorar el rendimiento y generación de reportes en PostgreSQL.",
+        "Lideré y participé en un proyecto de gestión logística, ayudando a definir la estructura del sistema, los flujos de trabajo y documentando de forma clara, además de realizar pruebas para asegurar calidad.",
         "Corrección de bugs, resolución de errores e incidencias, y mejora continua de los sistemas ERP.",
-        "Implementación de pruebas unitarias en PHP y documentación para garantizar la calidad.",
+        "Implementación de interfaces de usuario utilizando React.",
+        "Implementación de pruebas unitarias.",
+        "Desarrollo e implementación de APIs utilizando Node."
       ],
     },
     {
@@ -31,9 +32,8 @@ const data = {
       responsabilities: [
         "Desarrollo de funcionalidades en Python (Django) para recibir y procesar datos desde dispositivos ESP32",
         "Desarrollo de funcionalidades en OpenCV para la captura de imágenes desde cámaras, facilitando el preprocesamiento y la integración con herramientas de etiquetado ArUco.",
-        "Implementación de contenedores Docker para aplicaciones críticas, mejorando la portabilidad y acelerando los despliegues en entornos de desarrollo.",
-        "Desarrollo de interfaces de usuario y mejora de plantillas HTML y CSS para la proyección de mapas.",
-        "Desarrollo y etiquetado de zonas en QGIS, optimizando la preparación de datos geoespaciales para análisis posteriores.",
+        "Implementación de contenedores Docker para aplicaciones críticas.",
+        "Desarrollo de interfaces de para la proyección de mapas.",
       ],
     },
     {
@@ -43,17 +43,19 @@ const data = {
       location: "Concepción",
       responsabilities: [
         "Asistencia en la supervisión de una flota de vehículos, incluyendo la programación de mantenimientos y el monitoreo mediante Excel.",
-        "Comunicación con proveedores de automóviles para validar siniestros y coordinar la reparación de vehículos.",
-        "Organización y clasificación de documentos, asegurando una gestión eficiente y un acceso rápido a la información requerida.",
+        "Comunicación con proveedores de automóviles para validar siniestros.",
+        "Organización y clasificación de documentos."
       ],
     },
   ],
   hard_skills: [
     "Python",
     "PHP",
-    "JavaScript/React",
+    "JavaScript",
     "PostgreSQL",
-    "Testing unitario (básico)",
+    "React",
+    "Testing unitario",
+    "Cobol"
   ],
   education: [
     {
